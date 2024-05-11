8681cf0a71c4f521054f83d78d78102300410c9d
wnghks7787 <wnghks7787@naver.com>
2024-05-11 16:22:49 +0900

    add SLEEPING state in Light.java

--- src/Light.java
+++ src/Light.java
@@ -1,6 +1,7 @@
 public class Light {
     private static int ON = 0;  // LightBulb state == ON
     private static int OFF = 1; // LightBulb state == OFF
+    private static int SLEEPING = 2;    // Lightbulb state == SLEEPING
     private int state;          // Current state of Lightbulb
 
     public Light() {
@@ -9,7 +10,11 @@ public class Light {
 
     public void on_button_pushed() {
         if(state == ON) {
-            System.out.println("Nothing Change");
+            System.out.println("Sleeping Mode");
+            state = SLEEPING;
+        } else if(state == SLEEPING) {  //  If state is sleeping when on button pushed, change state to on.
+            System.out.println("Light ON!");
+            state = ON;
         } else {    // If state is off when on button pushed, change state to on.
             System.out.println("Light ON!");
             state = ON;
@@ -19,6 +24,9 @@ public class Light {
     public void off_button_pushed() {
         if(state == OFF) {
             System.out.println("Nothing Change");
+        } else if(state == SLEEPING) {  //  If state is sleeping when off button pushed, change state to off.
+            System.out.println("Light OFF!");
+            state = OFF;
         } else {    // If state is on when off button pushed, change state to off.
             System.out.println("Light OFF!");
             state = OFF;
